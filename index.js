44c9e925800a4ea3e436133164acb812fed8d513
Ian Sodersjerna <53672609+plop91@users.noreply.github.com>
2022-02-16 15:53:38 -0500

    Add tutorial code from https://discordjs.guide
    Added tutorial code from https://discordjs.guide, each user will need to register a bot at https://discord.com/developers and create a config.json with the requisite information

---
+++ index.js
@@ -0,0 +1,23 @@
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+    console.log(interaction);
+    if (!interaction.isCommand()) return;
+
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Pong!');
+    }
+});
+
+// Login to Discord with your client's token
+client.login(token);
