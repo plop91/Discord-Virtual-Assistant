21954e858f040c014c79baa61429a4f318479f1c
Ian Sodersjerna <iansodersjerna@gmail.com>
2022-02-16 17:34:00 -0500

    add discord.js/voice dependencies and single server join and leave commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,17 +1,17 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('./config.json');
+const { clientId, token } = require('./config.json');
 
 const commands = [
-    new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-    new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-    new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!!'),
+	new SlashCommandBuilder().setName('join').setDescription('bot joins audio channel'),
+	new SlashCommandBuilder().setName('leave').setDescription('bot leaves audio channel'),
 ]
-    .map(command => command.toJSON());
+	.map(command => command.toJSON());
 
 const rest = new REST({ version: '9' }).setToken(token);
 
 rest.put(Routes.applicationCommands(clientId), { body: commands })
-    .then(() => console.log('Successfully registered application commands.'))
-    .catch(console.error);
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 // Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
+const { joinVoiceChannel } = require('@discordjs/voice');
 const { token } = require('./config.json');
+
+let connection;
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
-    console.log('Ready!');
+	console.log('Ready!');
 });
 
 client.on('interactionCreate', async interaction => {
-    console.log(interaction);
-    if (!interaction.isCommand()) return;
+	if (!interaction.isCommand()) return;
 
-    if (interaction.commandName === 'ping') {
-        await interaction.reply('Pong!');
-    }
+	if (interaction.commandName === 'ping') {
+		await interaction.reply('Pong!!');
+	}
+	if (interaction.commandName === 'join') {
+		await interaction.reply('joining');
+		const channel = interaction.member.voice.channel;
+		connection = joinVoiceChannel({
+			channelId: channel.id,
+			guildId: channel.guild.id,
+			adapterCreator: channel.guild.voiceAdapterCreator,
+		});
+		console.log(connection);
+	}
+	if (interaction.commandName === 'leave') {
+		connection.destroy();
+	}
 });
 
 // Login to Discord with your client's token
-client.login(token);
+client.login(token)
+	.then(() => console.log('App starting'))
+	.catch(console.error);
