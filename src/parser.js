f6578088fcbe2ac4db3591d605aa8fcc787143ea
Jacob Austin <jacobaustin.jma@gmail.com>
2022-03-16 15:41:58 -0400

    Fixed ESLint issues

--- src/parser.js
+++ src/parser.js
@@ -8,35 +8,35 @@
  @Changelog:
 2/19/2022 IS: Added basic structure
  */
-const DiscordHandler = require("./discord")
-const T2S = require("./t2s");
+const DiscordHandler = require('./discord');
+const T2S = require('./t2s');
 
-class ParserGeneric{
-    constructor(discord) {
-        this.discord = discord;
-        this.text2speech = new T2S();
-    }
-    parse(text){
-        return -1
-    }
+class ParserGeneric {
+	constructor(discord) {
+		this.discord = discord;
+		this.text2speech = new T2S();
+	}
+	parse(text) {
+		return -1;
+	}
 }
-class Parser extends ParserGeneric{
-    constructor(discord) {
-        super(discord);
-    }
-    parse(text){
-        return 0
-    }
+class Parser extends ParserGeneric {
+	constructor(discord) {
+		super(discord);
+	}
+	parse(text) {
+		return 0;
+	}
 
 }
-class ParserTest extends ParserGeneric{
-    constructor(discord) {
-        super(discord);
-    }
-    parse(text){
-        //do test thing
-        return 0
-    }
+class ParserTest extends ParserGeneric {
+	constructor(discord) {
+		super(discord);
+	}
+	parse(text) {
+		// do test thing
+		return 0;
+	}
 
 }
 module.exports = Parser;
