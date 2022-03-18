01690890857bcf2b8fe9825f5b991ee0cfa46738
Nicholas_Miceli <Nicholas_miceli@student.uml.edu>
2022-03-18 11:17:07 -0400

    Added tests for t2s

--- tests/t2s.test.js
+++ tests/t2s.test.js
@@ -6,15 +6,37 @@
  @Description: Unit testing for Text-to-Speech interface.
 
  @Changelog:
+ 3/17/2022 NM:Added tests for matching and not matching audio
  2/19/2022 IS:Added constructor test
  */
 const T2S = require('../src/t2s');
-/*
-Tests for text to speech module will go here
- */
+const fs = require('fs');
+// eslint-disable-next-line no-undef
+jest.useFakeTimers('legacy');
+
 
 // eslint-disable-next-line no-undef
-test('Discord constructor', () => {
+test('Synthesize speech match', async () => {
+	const text2Speech = new T2S();
+	await text2Speech.convert('Sorry, I don\'t understand.');
+	const resBuffer = fs.readFileSync('tests/resources/testAudioContent1.txt');
+	const testBuffer = text2Speech.lastAudioContent;
+	// eslint-disable-next-line no-undef
+	expect(Buffer.compare(resBuffer, testBuffer) == 0).toBe(true);
+});
+
+// eslint-disable-next-line no-undef
+test('Synthesize speech no match', async () => {
+	const text2Speech = new T2S();
+	await text2Speech.convert('Message sent.');
+	const resBuffer = fs.readFileSync('tests/resources/testAudioContent1.txt');
+	const testBuffer = text2Speech.lastAudioContent;
+	// eslint-disable-next-line no-undef
+	expect(Buffer.compare(resBuffer, testBuffer) == 0).toBe(false);
+});
+
+// eslint-disable-next-line no-undef
+test('T2S constructor', () => {
 	const text2speech = new T2S();
 	// eslint-disable-next-line no-undef
 	expect(text2speech).toBeDefined();
