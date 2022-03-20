6cc110e1404c66660836045d6df959e207e0bc02
Jonathan Tucker <jtuck2k@gmail.com>
2022-03-20 12:55:26 -0400

    Converted test audio to .opus

--- tests/s2t.test.js
+++ tests/s2t.test.js
@@ -21,18 +21,18 @@ test('S2T constructor', () => {
 
 test('S2T "Hey Discord bot"', () => {
     const speech2text = new S2T();
-    const result = speech2text.transcribe("./tests/resources/s2tTestAudio1.flac");
+    const result = speech2text.transcribe("./tests/resources/s2tTestAudio1.opus");
     expect(result).toBe("Hey Discord bot");
 });
 
 test('S2T "Whats the weather like today"', () => {
     const speech2text = new S2T();
-    const result = speech2text.transcribe("./tests/resources/s2tTestAudio2.flac");
+    const result = speech2text.transcribe("./tests/resources/s2tTestAudio2.opus");
     expect(result).toBe("Whats the weather like today");
 });
 
 test('S2T "What time is the basketball game today"', () => {
     const speech2text = new S2T();
-    const result = speech2text.transcribe("./tests/resources/s2tTestAudio3.flac");
+    const result = speech2text.transcribe("./tests/resources/s2tTestAudio3.opus");
     expect(result).toBe("What time is the basketball game today");
 });
